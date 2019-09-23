0369c7698b2059637a2c6241671ecf068938bb9d
DiegoKraenau <diegokraenau@gmail.com>
2019-09-23 01:00:13 -0500

    Navbar y vista Login

---
+++ RapiSolverWeb/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RapiSolverWeb.Models;
+
+namespace RapiSolverWeb.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
